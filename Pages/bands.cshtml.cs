c102cc621d64fef6d7285698808136c6d2b739cb
anas-ahm <anasmajid.ahm@gmail.com>
2026-03-16 22:44:16 +0100

    first commit

---
+++ Pages/bands.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace rockmuseet_og_gasolin.Pages.content;
+
+public class BandsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Pages/bands/gasolin.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace rockmuseet_og_gasolin.Pages.bands;
+
+public class GasolinModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
